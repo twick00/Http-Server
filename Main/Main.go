33a1c064f2ec8d56ac2ff82fbc046be58446d01a
twick00 <twick00@gmail.com>
2017-05-28 11:26:04 -0700

    Fixed bug when pressing button it took you to an undefined URL

--- Main/Main.go
+++ Main/Main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"hello/Main/more"
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/httprouter"
 )
 
 var data = struct {
-	Title  string
-	Items  []string
-	Yesman string
+	Title    string
+	Items    []string
+	Yesman   string
+	Comments []string
 }{
 	Title: "Video Games",
 	Items: []string{
@@ -18,20 +20,41 @@ var data = struct {
 		"Disgaea 5",
 		"World of Warcraft",
 	},
+	Comments: []string{
+		"Test",
+	},
 	Yesman: "",
 }
 
+func gethome(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	t, _ := template.ParseFiles("./More/content.html")
+	s1 := t.Lookup("content.html")
+	s1.ExecuteTemplate(rw, "newcomment", data)
+}
+func getroot(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	t, _ := template.ParseFiles("./More/content.html")
+	s1 := t.Lookup("content.html")
+	s1.ExecuteTemplate(rw, "content", data)
+}
+func posthome(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	comment := r.FormValue("comment")
+	data.Comments = append(data.Comments, comment)
+	log.Printf(comment)
+	gethome(rw, r, nil)
+}
+
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		t, _ := template.New("webpage").Parse(Templates.Tpl)
-		data.Yesman = "Not Jumped"
-		t.Execute(rw, data)
-	})
-	http.HandleFunc("/jump", func(rw http.ResponseWriter, r *http.Request) {
-		t, _ := template.New("webpage").Parse(Templates.Tpl)
+	router := httprouter.New()
+	router.GET("/", getroot)
+
+	router.GET("/home", gethome)
+
+	router.POST("/home", posthome)
+
+	/*http.HandleFunc("/jump", func(rw http.ResponseWriter, r *http.Request) {
+		t, _ := template.New("webpage").Parse()
 		data.Yesman = "Jumped"
 		t.Execute(rw, data)
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	})*/
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
