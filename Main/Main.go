8d62c3f03ed3564a6f4ac67b69fd2518030951fd
twick00 <twick00@gmail.com>
2017-05-31 11:47:12 -0700

    Removed JSON support, replaced with MySql for now.

--- Main/Main.go
+++ Main/Main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/httprouter"
 )
+
+var db *sql.DB
 
 // type dataContainer struct {
 // 	Title    string
@@ -32,42 +33,57 @@ func check(e error) { //Simple error passing
 	}
 }
 
-func decodejson() {
-	jlist, _ := ioutil.ReadFile("./More/list.json")
-	var marshjlist = []byte(jlist)
-	err := json.Unmarshal(marshjlist, &data)
-	check(err)
+// func decodejson() {
+// 	jlist, _ := ioutil.ReadFile("./More/list.json")
+// 	var marshjlist = []byte(jlist)
+// 	err := json.Unmarshal(marshjlist, &data)
+// 	check(err)
+// }
+// func encodejson() {
+// 	marshjlist, err := json.Marshal(data)
+// 	check(err)
+// 	fmt.Println(marshjlist)
+// 	ioutil.WriteFile("./More/list.json", marshjlist, os.ModeTemporary)
+// 	//Note: What is mode temp vs perm?
+// }
+
+//Game is game
+type Game struct {
+	Title string
+	Year  int
+	Genre string
 }
-func encodejson() {
-	marshjlist, err := json.Marshal(data)
-	check(err)
-	fmt.Println(marshjlist)
-	ioutil.WriteFile("./More/list.json", marshjlist, os.ModeTemporary)
-	//Note: What is mode temp vs perm?
+
+//Gamesstruct Contains []Game
+type Gamesstruct struct {
+	Games []Game
 }
 
 func gethome(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	decodejson()
+	//decodejson()
 	t, _ := template.ParseFiles("./More/content.html")
 	s1 := t.Lookup("content.html")
 	s1.ExecuteTemplate(rw, "newcomment", data)
 }
 func getroot(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	decodejson()
-	t, _ := template.ParseFiles("./More/content.html")
+	//decodejson()
+	var data Gamesstruct
+	data.Games = gettabledata()
+	log.Print(data.Games[0].Title)
+	t, err := template.ParseFiles("./More/content.html")
+	check(err)
 	s1 := t.Lookup("content.html")
 	s1.ExecuteTemplate(rw, "title", data)
-
 }
 func getgames(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	decodejson()
+	//decodejson()
 	t, _ := template.ParseFiles("./More/content.html")
 	s1 := t.Lookup("content.html")
 	fmt.Println(data.Games)
 	s1.ExecuteTemplate(rw, "games", data)
 }
 func getcomments(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	decodejson()
+	//decodejson()
 	t, _ := template.ParseFiles("./More/content.html")
 	s1 := t.Lookup("content.html")
 	fmt.Println(data.Comments)
@@ -75,17 +91,44 @@ func getcomments(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func posthome(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	decodejson()
+	//decodejson()
 	comment := r.FormValue("comment")
 	if comment != "" { //Can create empty strings when pressing the button
 		data.Comments = append(data.Comments, comment)
 	}
 	fmt.Println(data.Comments)
-	encodejson()
+
+	//encodejson()
 	gethome(rw, r, nil) //Fix later
+}
+func gettabledata() []Game {
+	games := []Game{} //All the data
+	game := Game{
+		Title: "",
+		Year:  0,
+		Genre: "",
+	} //Just one set of data
+	rows, err := db.Query("SELECT * FROM games") //
+	check(err)
+	for rows.Next() {
+		err := rows.Scan(&game.Title, &game.Year, &game.Genre)
+		games = append(games, game)
+		check(err)
+	}
+	return games
+}
+func connectdb() *sql.DB {
+	var err error
+	if db == nil {
+		db, err = sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/data") //PASSWORD!
+		check(err)
+		return db
+	}
+	return db
 }
 
 func main() {
+	connectdb()
 	router := httprouter.New()
 	router.GET("/", getroot)
 	router.GET("/home", gethome)
