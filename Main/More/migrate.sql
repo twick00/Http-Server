b0da2ee5b411baac0ffa705b5539795cf9f16c02
Tyler Wickline <tyler@cdinw.com>
2017-06-05 07:32:54 -0700

    Stability and readability changes. Migrate script fully implemented. Should now work out-of-the-box with MySql if username = root & password = password

--- Main/More/migrate.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS test;
-USE test;
-CREATE TABLE IF NOT EXISTS games ( 
-	title varchar(45),
-    year int(4),
-    genre varchar(45),
-    barcode int(18),
-	id int(20)
-    );
